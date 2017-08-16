5c15bb4511e359dde3dbed6a4f1ee71d31c5620e
rivarub <rivarubg@gmail.com>
2017-08-16 22:29:16 +0300

    body font-size, small bags out

--- js/js.js
+++ js/js.js
@@ -1,21 +1,23 @@
 $(document).ready(function () {
     $(window).scroll(function ()
     {
-        if ($(window).scrollTop() > 70) {
+        if ($(window).scrollTop() > 90) {
+            $('header').css('display', 'none');
             $('nav').css('position', 'fixed').css('top', '0').css('width', '100%');
-            $('.cd-bg-1').css('padding-top', '70px');
-            $('.nav-bottom').css('display', 'block').css('position', 'fixed').css('top', '54px');
+            $('.cd-bg-1').css('padding-top', '180px');
+//            $('.nav-bottom').css('display', 'block').css('position', 'fixed').css('top', '54px');
         } else {
+            $('header').css('display', 'block');
             $('nav').css('position', 'static');
             $('.cd-bg-1').css('padding-top', '0');
-            $('.nav-bottom').css('display', 'none');
+//            $('.nav-bottom').css('display', 'none');
         }
-        if ($(window).scrollTop() > 70 && $(window).scrollTop() < 1200){
+        if ($(window).scrollTop() > 70 && $(window).scrollTop() < 1150){
             $('.about').css('color', '#aa0019');
         } else {
             $('.about').css('color', '#484047');
         }
-        if ($(window).scrollTop() > 1199 && $(window).scrollTop() < 2200) {
+        if ($(window).scrollTop() > 1149 && $(window).scrollTop() < 2200) {
             $('.serv').css('color', '#aa0019');
         } else {
             $('.serv').css('color', '#484047');
