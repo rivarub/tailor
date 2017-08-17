79dc9ce694254d3cb1abec66d2484a7724ecc46e
rivarub <rivarubg@gmail.com>
2017-08-17 16:26:00 +0300

    landing-banner for dasktop

--- js/js.js
+++ js/js.js
@@ -1,28 +1,28 @@
 $(document).ready(function () {
     $(window).scroll(function ()
     {
-        if ($(window).scrollTop() > 90) {
-            $('header').css('display', 'none');
+        if ($(window).scrollTop() > 0) {
+            //$('header').css('display', 'none');
             $('nav').css('position', 'fixed').css('top', '0').css('width', '100%');
-            $('.cd-bg-1').css('padding-top', '180px');
+            //$('.cd-bg-1').css('padding-top', '100px');
 //            $('.nav-bottom').css('display', 'block').css('position', 'fixed').css('top', '54px');
         } else {
-            $('header').css('display', 'block');
+            //$('header').css('display', 'block');
             $('nav').css('position', 'static');
-            $('.cd-bg-1').css('padding-top', '0');
+            //$('.cd-bg-1').css('padding-top', '0');
 //            $('.nav-bottom').css('display', 'none');
         }
-        if ($(window).scrollTop() > 70 && $(window).scrollTop() < 1150){
+        if ($(window).scrollTop() > 800 && $(window).scrollTop() < 1950){
             $('.about').css('color', '#aa0019');
         } else {
             $('.about').css('color', '#484047');
         }
-        if ($(window).scrollTop() > 1149 && $(window).scrollTop() < 2200) {
+        if ($(window).scrollTop() > 1949 && $(window).scrollTop() < 3100) {
             $('.serv').css('color', '#aa0019');
         } else {
             $('.serv').css('color', '#484047');
         }
-        if ($(window).scrollTop() > 2199) {
+        if ($(window).scrollTop() > 3199) {
             $('.contacts').css('color', '#aa0019');
         } else {
             $('.contacts').css('color', '#484047');
