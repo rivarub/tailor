7dfe453e8724d0708f0fcf3894c8ea36acab8030
Roman Pryshlyak <pryshlyak@gmail.com>
2018-01-13 21:29:59 +0200

    each picture has onclick listener to be enlarged

--- js/js.js
+++ js/js.js
@@ -116,7 +116,7 @@ $(document).ready(function () {
     // Get the modal
     var modal = document.getElementById('myModal');
     //var modal1 = document.getElementById('myModal1');
-// Get the image and insert it inside the modal - use its "alt" text as a caption
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
     var img = document.getElementById('myImg');
     var modalImg = document.getElementById("img01");
     var captionText = document.getElementById("caption");
@@ -205,9 +205,15 @@ $(document).ready(function () {
     var i = 0;
     $('img', 'div.gal').each(function () {
         $(this).attr('id', 'img' + i);
-        console.log(i);
+        //console.log(i);
         $(this).attr("src", myImages[i].src);
         $(this).attr("alt", myImages[i].alt);
+        $(this).click(function(){
+            //alert(this.id);
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+        });
         i++;
     });
 
