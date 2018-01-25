64a643f59ade766e072937ea8585797449502317
Roman Pryshlyak <pryshlyak@gmail.com>
2018-01-25 02:37:15 +0200

    Better format of ukr email

--- sendMail.php
+++ sendMail.php
@@ -1,5 +1,5 @@
 <?php
-$debug = true;
+$debug = false;
 $name = $_POST['name'];
 $subject = "New Email from your site";
 $email = $_POST["email"];
@@ -17,7 +17,7 @@ $backMessage = 'Your message has been sent. Thank you! '
 . '<a href="index.html">Return to site </a>';
 
 if($_POST["lang"] == "Ukrainian") {
-    $backMessage = 'Ваш лист успішно пішов. Дякуємо! <a href="index_ua.html">Повернутися </a>';
+    $backMessage = '<html><head><title>Якісні та швидкі послуги від Vlada Professional Tailoring</title><meta charset="UTF-8"></head><body>Ваш лист успішно пішов. Дякуємо! <a href="index_ua.html">Повернутися </a></body></html>';
 } 
 
 mail($to,$subject,$message,$headers);
